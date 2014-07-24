a5c0fdbb2af0bbd3172be95fd66fc73964f1fd64
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-07-24 09:39:57 +0400

    Extract test utils to compare xmls

--- src/test/scala/IntegrationSuite.scala
+++ src/test/scala/IntegrationSuite.scala
@@ -1,13 +1,11 @@
 package faba.test
 
 import java.io.File
-import javax.xml.parsers.DocumentBuilderFactory
 
 import faba.Main
-import org.custommonkey.xmlunit.Diff
 import org.scalatest.FunSuite
 
-class IntegrationSuite extends FunSuite {
+class IntegrationSuite extends FunSuite with IntegrationSuiteUtil {
 
   test("jdk7-rt.jar") {
     Main.main(
@@ -16,26 +14,4 @@ class IntegrationSuite extends FunSuite {
     compareOutputs(new File("results/jdk7-rt.jar"), new File("test-results/jdk7-rt.jar"))
   }
 
-  def compareOutputs(dir1: File, dir2: File) {
-    val paths = annotationXmlPaths(dir1).toSet.toList.sorted
-    val actualPaths = annotationXmlPaths(dir2).toSet.toList.sorted
-    assert(paths == actualPaths)
-    paths.foreach {p => compareXMLs(new File(dir1, p), new File(dir2, p))}
-  }
-
-  def annotationXmlPaths(dir: File): Array[String] = {
-    def getRelativePath(dir: File, file: File) =
-      file.getAbsolutePath.substring(dir.getAbsolutePath.length() + 1)
-    def listFiles(f: File): Array[File] = {
-      val these = f.listFiles
-      these.filterNot(_.isDirectory) ++ these.filter(_.isDirectory).flatMap(listFiles)
-    }
-    listFiles(dir).filter(_.getName == "annotations.xml").map(getRelativePath(dir, _))
-  }
-
-  def compareXMLs(f1: File, f2: File) {
-    val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-    val diff = new Diff(builder.parse(f1), builder.parse(f2))
-    assert(diff.similar(), s"files ${f1.getAbsolutePath} and ${f2.getAbsolutePath} have different content")
-  }
 }
---
+++ src/test/scala/IntegrationSuiteUtil.scala
@@ -0,0 +1,32 @@
+package faba.test
+
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+
+import org.custommonkey.xmlunit.Diff
+import org.scalatest.Assertions
+
+trait IntegrationSuiteUtil extends Assertions {
+  def compareOutputs(dir1: File, dir2: File) {
+    val paths = annotationXmlPaths(dir1).toSet.toList.sorted
+    val actualPaths = annotationXmlPaths(dir2).toSet.toList.sorted
+    assert(paths == actualPaths)
+    paths.foreach {p => compareXMLs(new File(dir1, p), new File(dir2, p))}
+  }
+
+  def annotationXmlPaths(dir: File): Array[String] = {
+    def getRelativePath(dir: File, file: File) =
+      file.getAbsolutePath.substring(dir.getAbsolutePath.length() + 1)
+    def listFiles(f: File): Array[File] = {
+      val these = f.listFiles
+      these.filterNot(_.isDirectory) ++ these.filter(_.isDirectory).flatMap(listFiles)
+    }
+    listFiles(dir).filter(_.getName == "annotations.xml").map(getRelativePath(dir, _))
+  }
+
+  def compareXMLs(f1: File, f2: File) {
+    val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+    val diff = new Diff(builder.parse(f1), builder.parse(f2))
+    assert(diff.similar(), s"files ${f1.getAbsolutePath} and ${f2.getAbsolutePath} have different content")
+  }
+}
