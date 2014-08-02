60ec78421e052d40db964c81518cf7f3c56e37b4
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-08-03 01:13:18 +0400

    more precise information about hash collisions

--- src/main/scala/experimental/hasher.scala
+++ src/main/scala/experimental/hasher.scala
@@ -4,6 +4,7 @@ import java.io.File
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file._
 
+import faba.data.Method
 import faba.source.{MixedSource, JarFileSource, Source, Processor}
 import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.{ClassVisitor, ClassReader}
@@ -18,11 +19,14 @@ object Hasher extends Processor {
   var classes = Map[H, String]()
   var classCollisions = List[(String, String)]()
 
+  import java.security.MessageDigest
+  val messageDigest = MessageDigest.getInstance("MD5")
+
   def hashString(s: String): H = {
-    import java.security.MessageDigest
-    val messageDigest = MessageDigest.getInstance("MD5")
-    messageDigest.update(s.getBytes)
-    new String(messageDigest.digest())
+
+    val digest = messageDigest.digest(s.getBytes)
+    //new String(digest.take(8))
+    digest.take(10).mkString(",")
   }
 
   override def processClass(classReader: ClassReader) {
@@ -37,10 +41,12 @@ object Hasher extends Processor {
     classes += classHash -> className
 
     classReader.accept(new ClassVisitor(ASM5) {
-      var hashes = Map[Int, String]()
+      var hashes = Map[String, String]()
       override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]) = {
+        val method = Method(className, name, desc)
+
         val mDesc = s"$name$desc"
-        val hash = mDesc.hashCode
+        val hash = messageDigest.digest(mDesc.getBytes).take(4).mkString(",")
         if (hashes.contains(hash)) {
           println(s"${classReader.getClassName} ${hashes(hash)} $mDesc")
           collisions += 1
@@ -74,7 +80,7 @@ object Hasher extends Processor {
       JarFileSource(new File(args(0))).process(this)
     }
     println(s"classes:    $classesN")
-    println(s"methodCollisions: $collisions")
+    println(s"# of methodCollisions: $collisions")
     println(s"classCollisions: $classCollisions")
     println(s"# of classCollisions: ${classCollisions.size}")
   }
