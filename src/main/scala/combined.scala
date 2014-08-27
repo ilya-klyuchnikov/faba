90d3f2c63e2afb55ffa9e8e177c524fe6178723a
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-08-27 14:59:10 +0400

    CombinedAnalysis cleanup

--- src/main/scala/combined.scala
+++ src/main/scala/combined.scala
@@ -12,8 +12,8 @@ import org.objectweb.asm.tree.analysis.{BasicInterpreter, BasicValue, Frame}
 import scala.annotation.switch
 import scala.collection.JavaConversions._
 
-case class CombinedCall(tp: Type, method: Method, stableCall: Boolean, args: List[_ <: BasicValue]) extends BasicValue(tp)
-case class NParamValue(tp: Type, n: Int) extends BasicValue(tp)
+case class CallValue(tp: Type, method: Method, stableCall: Boolean, args: List[_ <: BasicValue]) extends BasicValue(tp)
+case class NthParamValue(tp: Type, n: Int) extends BasicValue(tp)
 
 // specialized class for analyzing methods without branching
 // this is a good tutorial example
@@ -55,13 +55,13 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
   def notNullParamEquation(i: Int, stable: Boolean): Equation[Key, Value] = {
     val key = Key(method, In(i), stable)
     val result: Result[Key, Value] =
-      if (interpreter.dereferenced(i)) Final(Values.NotNull)
+      if (interpreter.dereferencedParams(i)) Final(Values.NotNull)
       else {
-        val calls: Set[Key] = interpreter.callDerefs.getOrElse(i, Set())
-        if (calls.isEmpty)
+        val used = interpreter.parameterFlow.getOrElse(i, Set())
+        if (used.isEmpty)
           Final(Values.Top)
         else
-          Pending[Key, Value](Set(Component(Values.Top, calls)))
+          Pending[Key, Value](Set(Component(Values.Top, used)))
       }
     Equation(key, result)
   }
@@ -69,13 +69,13 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
   def nullableParamEquation(i: Int, stable: Boolean): Equation[Key, Value] = {
     val key = Key(method, In(i), stable)
     val result: Result[Key, Value] =
-      if (interpreter.dereferenced(i) || interpreter.notNullable(i)) Final(Values.Top)
+      if (interpreter.dereferencedParams(i) || interpreter.notNullableParams(i)) Final(Values.Top)
       else {
         returnValue match {
-          case NParamValue(_, `i`) =>
+          case NthParamValue(_, `i`) =>
             Final(Values.Top)
           case _ =>
-            val calls: Set[Key] = interpreter.callDerefs.getOrElse(i, Set())
+            val calls: Set[Key] = interpreter.parameterFlow.getOrElse(i, Set())
             if (calls.isEmpty)
               Final(Values.Null)
             else
@@ -89,7 +89,7 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
   def contractEquation(paramIndex: Int, inValue: Value, stable: Boolean): Equation[Key, Value] = {
     val key = Key(method, InOut(paramIndex, inValue), stable)
     val result: Result[Key, Value] =
-      if (exception || (inValue == Values.Null && interpreter.dereferenced(paramIndex)))
+      if (exception || (inValue == Values.Null && interpreter.dereferencedParams(paramIndex)))
         Final(Values.Bot)
       else {
         returnValue match {
@@ -101,12 +101,12 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
             Final(Values.Null)
           case NotNullValue(_) =>
             Final(Values.NotNull)
-          case NParamValue(_, `paramIndex`) =>
+          case NthParamValue(_, `paramIndex`) =>
             Final(inValue)
-          case CombinedCall(retType, m, stableCall, args) =>
+          case CallValue(retType, m, stableCall, args) =>
             val isRefRetType = retType.getSort == Type.OBJECT || retType.getSort == Type.ARRAY
             var keys = Set[Key]()
-            for ((NParamValue(_, `paramIndex`), j) <- args.zipWithIndex) {
+            for ((NthParamValue(_, `paramIndex`), j) <- args.zipWithIndex) {
               keys += Key(m, InOut(j, inValue), stableCall)
             }
             if (isRefRetType) {
@@ -137,7 +137,7 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
             Final(Values.Null)
           case NotNullValue(_) =>
             Final(Values.NotNull)
-          case CombinedCall(_, m, stableCall, args) =>
+          case CallValue(_, m, stableCall, args) =>
             val callKey = Key(m, Out, stableCall)
             Pending[Key, Value](Set(Component(Values.Top, Set(callKey))))
           case _ =>
@@ -161,7 +161,7 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
       local += 1
     }
     for (i <- 0 until args.size) {
-      val value = new NParamValue(args(i), i)
+      val value = new NthParamValue(args(i), i)
       frame.setLocal(local, value)
       local += 1
       if (args(i).getSize == 2) {
@@ -177,14 +177,19 @@ class CombinedSingleAnalysis(val method: Method, val controlFlow: ControlFlowGra
   }
 }
 
-// Full interpreter, propagates everything
+// Full single-pass interpreter, propagates everything needed for analysis.
 case class CombinedInterpreter(arity: Int) extends BasicInterpreter {
-  // should be @NotNull
-  val dereferenced = new Array[Boolean](arity)
-  // cannot be nullable: passed to a field, array or to interface method
-  var notNullable = new Array[Boolean](arity)
-  // dereferenced via call
-  var callDerefs = Map[Int, Set[Key]]()
+  // Parameters dereferenced during execution of a method, tracked by parameter's indices.
+  // Dereferenced parameters are @NotNull.
+  val dereferencedParams = new Array[Boolean](arity)
+
+  // Parameters, that are written to something or passed to an interface methods.
+  // This parameters cannot be @Nullable.
+  var notNullableParams = new Array[Boolean](arity)
+
+  // parameterFlow(i) for i-th parameter stores a set parameter positions it is passed to
+  // parameter is @NotNull if any of its usages are @NotNull
+  var parameterFlow = Map[Int, Set[Key]]()
 
   override def newOperation(insn: AbstractInsnNode): BasicValue = {
     (insn.getOpcode: @switch) match {
@@ -217,12 +222,12 @@ case class CombinedInterpreter(arity: Int) extends BasicInterpreter {
   override def unaryOperation(insn: AbstractInsnNode, value: BasicValue): BasicValue = {
     (insn.getOpcode: @switch) match {
       case GETFIELD | ARRAYLENGTH | MONITORENTER =>
-        if (value.isInstanceOf[NParamValue])
-          dereferenced(value.asInstanceOf[NParamValue].n) = true
+        if (value.isInstanceOf[NthParamValue])
+          dereferencedParams(value.asInstanceOf[NthParamValue].n) = true
         super.unaryOperation(insn, value)
-      case CHECKCAST if value.isInstanceOf[NParamValue] =>
-        val nParam = value.asInstanceOf[NParamValue]
-        NParamValue(Type.getObjectType(insn.asInstanceOf[TypeInsnNode].desc), nParam.n)
+      case CHECKCAST if value.isInstanceOf[NthParamValue] =>
+        val nParam = value.asInstanceOf[NthParamValue]
+        NthParamValue(Type.getObjectType(insn.asInstanceOf[TypeInsnNode].desc), nParam.n)
       case NEWARRAY | ANEWARRAY =>
         NotNullValue(super.unaryOperation(insn, value).getType)
       case _ =>
@@ -233,13 +238,13 @@ case class CombinedInterpreter(arity: Int) extends BasicInterpreter {
   override def binaryOperation(insn: AbstractInsnNode, v1: BasicValue, v2: BasicValue): BasicValue = {
     (insn.getOpcode: @switch) match {
       case PUTFIELD =>
-        if (v1.isInstanceOf[NParamValue])
-          dereferenced(v1.asInstanceOf[NParamValue].n) = true
-        if (v2.isInstanceOf[NParamValue])
-          notNullable(v2.asInstanceOf[NParamValue].n) = true
+        if (v1.isInstanceOf[NthParamValue])
+          dereferencedParams(v1.asInstanceOf[NthParamValue].n) = true
+        if (v2.isInstanceOf[NthParamValue])
+          notNullableParams(v2.asInstanceOf[NthParamValue].n) = true
       case IALOAD | LALOAD | FALOAD | DALOAD | AALOAD | BALOAD | CALOAD | SALOAD =>
-        if (v1.isInstanceOf[NParamValue])
-          dereferenced(v1.asInstanceOf[NParamValue].n) = true
+        if (v1.isInstanceOf[NthParamValue])
+          dereferencedParams(v1.asInstanceOf[NthParamValue].n) = true
       case _ =>
     }
     super.binaryOperation(insn, v1, v2)
@@ -249,13 +254,13 @@ case class CombinedInterpreter(arity: Int) extends BasicInterpreter {
   override def ternaryOperation(insn: AbstractInsnNode, v1: BasicValue, v2: BasicValue, v3: BasicValue): BasicValue = {
     (insn.getOpcode: @switch) match {
       case IASTORE | LASTORE | FASTORE | DASTORE | BASTORE | CASTORE | SASTORE =>
-        if (v1.isInstanceOf[NParamValue])
-          dereferenced(v1.asInstanceOf[NParamValue].n) = true
+        if (v1.isInstanceOf[NthParamValue])
+          dereferencedParams(v1.asInstanceOf[NthParamValue].n) = true
       case AASTORE =>
-        if (v1.isInstanceOf[NParamValue])
-          dereferenced(v1.asInstanceOf[NParamValue].n) = true
-        if (v3.isInstanceOf[NParamValue])
-          notNullable(v3.asInstanceOf[NParamValue].n) = true
+        if (v1.isInstanceOf[NthParamValue])
+          dereferencedParams(v1.asInstanceOf[NthParamValue].n) = true
+        if (v3.isInstanceOf[NthParamValue])
+          notNullableParams(v3.asInstanceOf[NthParamValue].n) = true
       case _ =>
     }
     super.ternaryOperation(insn, v1, v2, v3)
@@ -267,8 +272,8 @@ case class CombinedInterpreter(arity: Int) extends BasicInterpreter {
     // catching explicit dereferences
     if (opCode == INVOKESPECIAL || opCode == INVOKEINTERFACE || opCode == INVOKEVIRTUAL) {
       values.get(0) match {
-        case np: NParamValue =>
-          dereferenced(np.n) = true
+        case np: NthParamValue =>
+          dereferencedParams(np.n) = true
         case _ =>
       }
     }
@@ -281,20 +286,20 @@ case class CombinedInterpreter(arity: Int) extends BasicInterpreter {
         val method = Method(mNode.owner, mNode.name, mNode.desc)
         for (i <- shift until values.size()) {
           values.get(i) match {
-            case np: NParamValue =>
+            case np: NthParamValue =>
               if (opCode == INVOKEINTERFACE) {
                 // no knowledge about interface
-                notNullable(np.n) = true
+                notNullableParams(np.n) = true
               }
               else {
-                val npKeys = callDerefs.getOrElse(np.n, Set())
+                val npKeys = parameterFlow.getOrElse(np.n, Set())
                 val key = Key(method, In(i - shift), stable)
-                callDerefs = callDerefs.updated(np.n, npKeys + key)
+                parameterFlow = parameterFlow.updated(np.n, npKeys + key)
               }
             case _ =>
           }
         }
-        CombinedCall(retType, method, stable, values.drop(shift).toList)
+        CallValue(retType, method, stable, values.drop(shift).toList)
       case MULTIANEWARRAY =>
         NotNullValue(super.naryOperation(insn, values).getType)
       case _ =>
