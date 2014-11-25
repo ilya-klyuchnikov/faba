6d59f392820577796099c05506e65f14345b5031
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-11-25 11:26:59 +0300

    draft version of aggregating resolve

--- src/main/scala/calls.scala
+++ src/main/scala/calls.scala
@@ -1,13 +1,29 @@
 package faba.calls
 
 import faba.data.Method
+import org.objectweb.asm.Opcodes
 
 import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-// class info got at indexing phase
+/**
+ * Class info available at indexing phase (in class file of a current file, without resolve)
+ * @param access     (org.objectweb.asm.tree.ClassNode#access)
+ * @param name       (org.objectweb.asm.tree.ClassNode#name)
+ * @param superName  (org.objectweb.asm.tree.ClassNode#superName)
+ * @param interfaces (org.objectweb.asm.tree.ClassNode#interfaces)
+ */
 case class ClassInfo(access: Int, name: String, superName: String, interfaces: List[String])
+
+/**
+ * Aggregated class info after resolve.
+ *
+ * @param classInfo     class info before resolve
+ * @param hierarchyLine hierarchy line (from this to Object)
+ * @param interfaces    a set of all interfaces this class implements
+ */
+case class ResolvedClassInfo(classInfo: ClassInfo, hierarchyLine: List[String], interfaces: Set[String])
 
 /**
  * Support for inference with hierarchy.
@@ -19,6 +35,7 @@ class CallResolver {
 
   // information collected during indexing
   private val classInfos = mutable.HashMap[String, ClassInfo]()
+  private val resolved = mutable.HashMap[String, ResolvedClassInfo]()
 
   /**
    * Calculates all concrete inheritors of this class.
@@ -66,39 +83,80 @@ class CallResolver {
   }
 
   /**
-   * Build hierarchy info. Call this method when indexing is completed.
+   * "At once" resolve.
    *
    * It materializes all classes and methods.
    * Builds the "hierarchy line" of classes for each class,
    * Builds the set of interface a class implements for each class.
    */
-  def buildHierarchy() {
+  def resolve() {
     indexing = false
+    // building resolve info only for classes (not interfaces)
+    for ((className, classInfo) <- classInfos if CallUtils.notInterface(classInfo.access))
+      resolved.update(className, ResolvedClassInfo(classInfo, hierarchy(className), allInterfaces(className)))
   }
 
   /**
    * Builds a hierarchy of parents for a class in a linear order (not including class itself).
    *
    * @param className class for which hierarchy is built
-   * @param acc alreadhy built hierarchy
    * @return hierarchy of the class
    */
-  @tailrec
-  private def hierarchy(className: String, acc: ListBuffer[String] = ListBuffer()): List[String] = {
-    classInfos.get(className) match {
-      case None =>
-        println(s"warning: $className is referenced, but not found")
-        acc.toList
-      case Some(classInfo) =>
-        val superName = classInfo.superName
-        if (superName == null)
+  private def hierarchy(className: String): List[String] = {
+    @tailrec
+    def _hierarchy(name: String, acc: ListBuffer[String] = ListBuffer()): List[String] = {
+      classInfos.get(name) match {
+        case None =>
+          println(s"warning: $name is referenced, but not found")
           acc.toList
-        else {
-          acc += superName
-          hierarchy(superName, acc)
-        }
-
+        case Some(classInfo) =>
+          val superName = classInfo.superName
+          if (superName == null)
+            acc.toList
+          else {
+            acc += superName
+            _hierarchy(superName, acc)
+          }
+      }
     }
+    _hierarchy(className)
   }
 
+  /**
+   * Calculates all interfaces implemented by a class.
+   *
+   * @param className class name
+   * @return all interfaces implemented by this class
+   */
+  private def allInterfaces(className: String): Set[String] = {
+    @tailrec
+    def _allInterfaces(queue: Set[String], traversed: Set[String], acc: Set[String]): Set[String] = {
+      queue.headOption match {
+        case None =>
+          acc
+        case Some(name) =>
+          classInfos.get(name) match {
+            case None =>
+              println(s"warning: $name is referenced, but not found")
+              acc
+            case Some(classInfo) =>
+              val interfaces = classInfo.interfaces.toSet
+              _allInterfaces(queue.tail ++ (interfaces -- traversed), traversed + name, acc ++ interfaces)
+          }
+      }
+    }
+    _allInterfaces(Set(className), Set(), Set())
+  }
+}
+
+/**
+ * Helper methods
+ */
+object CallUtils {
+  /**
+   * True if a class not an interface.
+   * @param access (org.objectweb.asm.tree.ClassNode#access)
+   */
+  def notInterface(access: Int) =
+    (access & Opcodes.ACC_INTERFACE) == 0
 }
