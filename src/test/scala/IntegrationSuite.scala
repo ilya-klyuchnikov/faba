2bf37e50e128846b4ba51b9af3d70b8f154ecae0
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2015-09-21 15:47:13 +0300

    update test to the latest JDK7

--- src/test/scala/IntegrationSuite.scala
+++ src/test/scala/IntegrationSuite.scala
@@ -9,7 +9,7 @@ class IntegrationSuite extends FunSuite with IntegrationSuiteUtil {
 
   test("jdk7-rt.jar") {
     Main.main(
-      Array("/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/jre/lib/rt.jar", "test-results/jdk7-rt.jar")
+      Array("/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/jre/lib/rt.jar", "test-results/jdk7-rt.jar")
     )
     compareOutputs(new File("results/jdk7-rt.jar"), new File("test-results/jdk7-rt.jar"))
   }
